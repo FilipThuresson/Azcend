b168b14f7073d3f06a7cb7afe6037b8f4fa73931
Filip Thuresson <60510154+FilipThuresson@users.noreply.github.com>
2023-06-25 00:23:59 +0200

    Changed readme and edited view file

--- src/App/controllerTemplate.php
+++ src/App/controllerTemplate.php
@@ -9,7 +9,7 @@ class '.$className.'Controller extends BaseController
 {
     public function index(): void
     {
-        $this->view(\''. strtolower($className) . '.' . strtolower($className) .'\');
+        $this->view(\''. strtolower($className) . '.index\');
     }
 }
     ';
