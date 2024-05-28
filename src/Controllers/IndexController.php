9e9f5668a092aaf3166dd3b231c00e0414fd7de1
Filip Thuresson <filip.tthuresson@gmail.com>
2024-05-28 23:16:03 +0200

    Composer in dev mode, added support for middlewares, please not inorder for this to work the BaseMiddleWare class from Azcend\Core must exist

--- src/Controllers/IndexController.php
+++ src/Controllers/IndexController.php
@@ -2,10 +2,19 @@
 
 namespace Azcend\Controllers;
 
+use Azcend\Core\Session;
+
 class IndexController extends BaseController
 {
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
     public function index(): void
     {
+
+        Session::set('user', ["name" => "Filip Thuresson", "email" => "filip.tthuresson@gmail.com"]);
         $this->view('index.index');
     }
 }
